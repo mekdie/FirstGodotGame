f0ec2aa481a3fce7e2264d16b27e02067cb051cc
Mekdie <mcdonyleee@gmail.com>
2025-09-24 17:39:46 +1000

    Added score, music, and sound effect on coin collection

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+@onready var game_manager = %GameManager
+@onready var animation_player = $AnimationPlayer
+
 func _on_body_entered(body: Node2D) -> void:
 	print("Coin collected!")
-	queue_free()
+	game_manager.add_point()
+	animation_player.play("pickup")
